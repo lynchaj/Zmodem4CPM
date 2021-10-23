0cea3baa69cc426baf79506092e9873d97d050a1
lynchaj <86925539+lynchaj@users.noreply.github.com>
2021-10-23 19:51:23 -0400

    Add files via upload

---
+++ Assembly/rzsz.bat
@@ -0,0 +1,7 @@
+rem assemble IDAFW sources
+tasm -t80 -b -f00 rz_ida.asm rz_ida.com
+tasm -t80 -b -f00 sz_ida.asm sz_ida.com
+rem
+rem assemble YAZD sources
+rem tasm -t80 -b -f00 rz_yazd.asm rz_yazd.com
+rem tasm -t80 -b -f00 sz_yazd.asm sz_yazd.com
