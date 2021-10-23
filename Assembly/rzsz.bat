2f2ac28d7f56ba9658fe1bdc12103cccbeaef2cc
lynchaj <86925539+lynchaj@users.noreply.github.com>
2021-10-23 19:55:06 -0400

    Update rzsz.bat

--- Assembly/rzsz.bat
+++ Assembly/rzsz.bat
@@ -5,3 +5,6 @@ rem
 rem assemble YAZD sources
 rem tasm -t80 -b -f00 rz_yazd.asm rz_yazd.com
 rem tasm -t80 -b -f00 sz_yazd.asm sz_yazd.com
+
+rem compare rz.com to RZ_IDA.COM goes here
+rem compare sz.com to SZ_IDA.COM goes here
