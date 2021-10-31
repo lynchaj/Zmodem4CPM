21650867e17343d482992aa444169e06813c60ac
lynchaj <86925539+lynchaj@users.noreply.github.com>
2021-10-31 06:43:48 -0400

    Delete build.sh

--- Source/build.sh
+++
@@ -1,2 +0,0 @@
-zcc +cpm -DSTANDALONE  -DAMALLOC sz.c zm.c  util.c tyme.c  common.c fileio.c stubs.c -o SZ.COM -create-app
-zcc +cpm -DSTANDALONE  -DAMALLOC rz.c zm.c  util.c tyme.c  common.c fileio.c stubs.c -o RZ.COM -create-app
